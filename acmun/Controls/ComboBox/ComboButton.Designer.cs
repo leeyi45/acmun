659e4e5ffb0ac65fa30f90d8c68afa04ee7983fb
leeyi45 <leeyi45@gmail.com>
2018-09-09 23:39:48 +0800

    Added new ComboButton Control
    Combines the combobox with the add button so that the button automatically changes position if the combobox is resized

---
+++ acmun/Controls/ComboBox/ComboButton.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace leeyi45.acmun.Controls
+{
+    partial class ComboButton
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new leeyi45.acmun.Controls.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(4, 0);
+            this.button1.MaximumSize = new System.Drawing.Size(24, 23);
+            this.button1.MinimumSize = new System.Drawing.Size(24, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(34, 1);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(326, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // ComboButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "ComboButton";
+            this.Size = new System.Drawing.Size(360, 25);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ acmun/Controls/ComboBox/ComboButton.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace leeyi45.acmun.Controls
+{
+    public partial class ComboButton : UserControl
+    {
+        public ComboButton()
+        {
+            InitializeComponent();
+
+            button1.Click += Button1_Click;
+
+            var size = TextRenderer.MeasureText("test", comboBox1.Font);
+            MinimumSize = new Size(comboBox1.MinimumSize.Width + 34, size.Height + 10);
+            //MaximumSize = new Size(comboBox1.MaximumSize.Width + 34, size.Height + 10);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            AddButtonClick?.Invoke(this, EventArgs.Empty);
+        }
+
+        public new Font Font
+        {
+            get => comboBox1.Font;
+            set
+            {
+                comboBox1.Font = value;
+                var size = TextRenderer.MeasureText("test", value);
+                Height = size.Height + 10;
+            }
+        }
+
+        public int ItemHeight
+        {
+            get => comboBox1.ItemHeight;
+            set => comboBox1.ItemHeight = value;
+        }
+
+        public ComboBox.ObjectCollection Items => comboBox1.Items;
+
+        public int SelectedIndex
+        {
+            get => comboBox1.SelectedIndex;
+            set => comboBox1.SelectedIndex = value;
+        }
+
+        public object SelectedItem
+        {
+            get => comboBox1.SelectedItem;
+            set => comboBox1.SelectedItem = value;
+        }
+
+        public void ResetItems(string[] items) => comboBox1.ResetItems(items);
+
+        protected override void SetBoundsCore(int x, int y, int width, int height, BoundsSpecified specified)
+        {            
+            base.SetBoundsCore(x, y, width, TextRenderer.MeasureText("test", Font).Height + 10, specified);
+        }
+
+        public event EventHandler AddButtonClick;
+    }
+}
